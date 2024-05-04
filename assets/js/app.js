eda42b3f9f0d800a20a4a11ae0d67ea15f38d7d9
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-05-04 12:00:20 +0530

    add back to top button svg

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ function openNav() {
     speed: 8000,
     pauseOnHover: true,
     cssEase: 'linear',
-    infinite: true,
+    infinite: true,    
     dots: false,
     variableWidth: true,
     slidesToShow: 5,
@@ -43,7 +43,6 @@ const y = new Date();
 let Year = y.getFullYear();
 document.getElementById("year2").innerHTML = Year
 
-
 //------------------------pre-loder
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("load", function () {
@@ -51,6 +50,26 @@ document.addEventListener("DOMContentLoaded", function () {
       let loadingOverlay = document.getElementById("loadingOverlay");
       loadingOverlay.style.display = "none";
       document.querySelector("body").classList.remove("overflow-hidden");
-    },1000);
+    },2000);
   }); 
 });
+// -----------------------back-to-top
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
