938c0db50c3f8f8a8906186c5f8609b8d8ce33cf
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-05-04 12:40:56 +0530

    document format

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
-  }
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
 
-   // --------slider
-   $('.firstslider').slick({
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 8000,
-    pauseOnHover: true,
-    cssEase: 'linear',
-    infinite: true,    
-    dots: false,
-    variableWidth: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
+// --------slider
+$('.firstslider').slick({
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 8000,
+  pauseOnHover: true,
+  cssEase: 'linear',
+  infinite: true,
+  dots: false,
+  variableWidth: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
 });
 
 const tabcontant = document.querySelectorAll(".tabcontant");
@@ -25,20 +25,19 @@ tabcontant.forEach(e => e.style.display = "none");
 firstcontant.style.display = "block";
 fistactivetab.classList.add("active");
 function clickhere(tabs) {
-    tabcontant.forEach(e => e.style.display = "none");
-    const selectcontant = document.getElementById(tabs)
-    selectcontant.style.display = "block";
-    const tabsactive = document.querySelectorAll(".allbtn button")
-    tabsactive.forEach(e => e.classList.remove("active"))
-    const selecttab = document.querySelector(`button[onclick="clickhere('${tabs}')"]`)
-    selecttab.classList.add("active")
+  tabcontant.forEach(e => e.style.display = "none");
+  const selectcontant = document.getElementById(tabs)
+  selectcontant.style.display = "block";
+  const tabsactive = document.querySelectorAll(".allbtn button")
+  tabsactive.forEach(e => e.classList.remove("active"))
+  const selecttab = document.querySelector(`button[onclick="clickhere('${tabs}')"]`)
+  selecttab.classList.add("active")
 }
 
 
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year
-
 const y = new Date();
 let Year = y.getFullYear();
 document.getElementById("year2").innerHTML = Year
@@ -50,16 +49,13 @@ document.addEventListener("DOMContentLoaded", function () {
       let loadingOverlay = document.getElementById("loadingOverlay");
       loadingOverlay.style.display = "none";
       document.querySelector("body").classList.remove("overflow-hidden");
-    },2000);
-  }); 
+    }, 2000);
+  });
 });
+
 // -----------------------back-to-top
-// Get the button
 let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
@@ -67,8 +63,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
